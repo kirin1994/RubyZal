0ddc0e3d752df7e46847e94f25a2a1aa7187cdfd
kirin1994 <apodlawski@gmail.com>
2017-01-22 07:51:25 +0100

    Validation + User

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170122024428) do
+ActiveRecord::Schema.define(version: 20170122061052) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -55,6 +55,7 @@ ActiveRecord::Schema.define(version: 20170122024428) do
     t.text     "Informacje_dodatkowe"
     t.datetime "created_at",           null: false
     t.datetime "updated_at",           null: false
+    t.integer  "account_id"
   end
 
   add_foreign_key "cars", "users"
